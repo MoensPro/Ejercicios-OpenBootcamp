2ff1cebf07be37f20765af71bb4cc8ca4bb67625
MoensPro <115664737+MoensPro@users.noreply.github.com>
2022-11-06 20:58:23 +0100

    Add files via upload

---
+++ Java Básico/Ejercicio 1/src/Ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int varNumerica = 1;
+        float varDecimal = 1.1f;
+        String varTexto = "Texto";
+        boolean varBooleano = true;
+
+        System.out.println("El tipo de dato numerico entero es: " + varNumerica);
+        System.out.println("El tipo de dato numerico decimal es: " + varDecimal);
+        System.out.println("El tipo de dato de texto es: " + varTexto);
+        System.out.println("El tipo de dato booleano es: " + varBooleano);
+    }
+}
