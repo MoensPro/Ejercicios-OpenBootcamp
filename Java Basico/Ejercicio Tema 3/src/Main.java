33739aa58f6b393779853364410553ec5d19a5b1
MoensPro <115664737+MoensPro@users.noreply.github.com>
2022-11-14 15:51:50 +0100

    Add files via upload

---
+++ Java Basico/Ejercicio Tema 3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Marco", "Annie", "Fabiana", "Vincenzo"};
+        for (int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+
+    }
+}
